ca9a3245f11c2ec0a7dc49fe927f321f1d043301
Chenxu <wang542@iu.edu>
2019-10-05 14:02:37 -0400

    fixed query param hardcoded parameters

--- src/voluntyrBackend/api/views.py
+++ src/voluntyrBackend/api/views.py
@@ -9,7 +9,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
 
 from .models import Event, Organization,Volunteer, EndUser
-from .serializers import EventsSerializer, ObtainTokenPairSerializer, OrganizationInfoSerializer
 
 from .serializers import EventsSerializer, ObtainTokenPairSerializer, OrganizationSerializer, VolunteerSerializer, EndUserSerializer
 
@@ -61,6 +60,7 @@ class ObtainTokenPairView(TokenObtainPairView):
 class EventsAPIView(generics.ListCreateAPIView):
 
     queryset = Event.objects.all()
+    queryset = Event.objects.filter(organization_id=2)
     serializer_class = EventsSerializer
 
     def list(self, request, *args, **kwargs):
@@ -158,7 +158,7 @@ class OrganizationInfoAPIView(generics.ListCreateAPIView):
     Class View for app to obtain organization information to populate
     organization dashboard
     """
-    queryset = Organization.objects.all()
+    queryset = Organization.objects.filter(end_user_id=2)
     serializer_class = OrganizationInfoSerializer
 
     # TODO: get id, same as get scope?
