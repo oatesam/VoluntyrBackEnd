d05a7a494367a484803b00dd49e2c56b27a5f0ae
Charlie <Charlie.frank72@gmail.com>
2019-10-10 12:43:38 -0400

    commit to rebase dev

--- src/voluntyrBackend/voluntyrBackend/settings/prod.py
+++ src/voluntyrBackend/voluntyrBackend/settings/prod.py
@@ -1,12 +1,15 @@
 from .shared import *
 
+# TODO: Set env var for secret_key
+SECRET_KEY = os.environ.get('VOL_SECRET_KEY')
 
-# ALLOWED_HOSTS = []
+# TODO: Add allowed hosts
+ALLOWED_HOSTS = []
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+# TODO: Setup PostgreSQL db on heroku and set settings here
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
