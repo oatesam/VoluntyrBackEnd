5791665b9e43e9eed0943a9bfc65fc430823470f
Charlie <Charlie.frank72@gmail.com>
2019-12-08 15:09:48 -0500

    WIP: Typing... added

--- src/voluntyrBackend/chat/routing.py
+++ src/voluntyrBackend/chat/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from chat.consumers import ChatConsumer, RoomConsumer
+from chat.consumers import ChatConsumer, RoomConsumer, TypingConsumer
 
 websocket_urlpatterns = [
     path('ws/chat/', ChatConsumer),
     path('ws/chat/<str:token>/<str:room_id>/', ChatConsumer),
+    path('ws/typing/<str:token>/<str:room_id>/', TypingConsumer),
     path('ws/online/<str:token>/', RoomConsumer),
 ]
