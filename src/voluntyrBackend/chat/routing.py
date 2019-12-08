e0b58cd6fa86c49365a5c0dec1f4b51c3bbe2a52
Charlie <Charlie.frank72@gmail.com>
2019-12-08 02:32:41 -0500

    Online/offline working

--- src/voluntyrBackend/chat/routing.py
+++ src/voluntyrBackend/chat/routing.py
@@ -4,5 +4,5 @@ from chat.consumers import ChatConsumer, RoomConsumer
 websocket_urlpatterns = [
     path('ws/chat/', ChatConsumer),
     path('ws/chat/<str:token>/<str:room_id>/', ChatConsumer),
-    path('ws/room/<str:token>/<str:room_id>/', RoomConsumer),
+    path('ws/online/<str:token>/', RoomConsumer),
 ]
