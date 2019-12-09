b8c477b38549fe17efe0617acd34ee095fc0364b
Charlie <Charlie.frank72@gmail.com>
2019-12-08 19:09:57 -0500

    Done done

--- src/voluntyrBackend/chat/exceptions.py
+++ src/voluntyrBackend/chat/exceptions.py
@@ -1,6 +1,5 @@
 class ClientError(Exception):
     """
-    TODO Story-57: Delete if unused
     Custom exception class that is caught by the websocket receive() handler and translated into a send back to the client.
     """
     def __init(self, code):
